f40403293f31aa8f820abfd3898d5b4c54ae3f50
Eric Dusabe <edusabe@rppa.gov.rw>
2024-06-10 17:31:58 +0200

    adapt date across DB to app

--- api/src/main/java/org/openmrs/module/appointmentsync/api/utils/Util.java
+++ api/src/main/java/org/openmrs/module/appointmentsync/api/utils/Util.java
@@ -9,7 +9,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Util {
@@ -19,12 +25,12 @@ public class Util {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             conn.setRequestMethod(crudType);
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0");
             conn.setRequestProperty("Content-Type", "application/json");
 
             OutputStream os = conn.getOutputStream();
             os.write(json.getBytes());
             os.flush();
-
             if (conn.getResponseCode() != 200) {
                 throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
             }
@@ -69,14 +75,16 @@ public class Util {
     }
 
     public static String convertObjectToJson(PatientAppointment pa) {
+        String jsonStr = null;
         try {
             ObjectMapper mapper = new ObjectMapper();
-            String jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(pa);
+            System.out.println("Object ========> " + pa.toString());
+            jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(pa);
             return jsonStr;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return jsonStr;
     }
 
     public static PatientAppointment convertJsonToObject(String json, Class c) {
@@ -88,5 +96,18 @@ public class Util {
             e.printStackTrace();
         }
         return null;
+    }
+    public static String convertToIsoDatetime(String dateStr) {
+        String nowAsISO = "";
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(dateStr);
+
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            nowAsISO = df.format(date);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        }
+        return nowAsISO;
     }
 }
